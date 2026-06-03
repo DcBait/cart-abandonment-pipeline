cde5193b73af017f8fbac171919dee2a59aca34e
DcBait <156874072+DcBait@users.noreply.github.com>
2026-06-03 17:14:25 +0800

    Refactor funnel stages to use descriptive names and orders

--- dbt_project/cart_abandonment/models/mart/mart_funnel.sql
+++ dbt_project/cart_abandonment/models/mart/mart_funnel.sql
@@ -5,6 +5,7 @@
         incremental_strategy='merge'
     )
 }}
+
 WITH sessions AS (
     SELECT * FROM {{ ref('int_sessions') }}
 ),
@@ -18,19 +19,15 @@ session_funnel AS (
         MAX(device_type)                                                     AS device_type,
         MAX(traffic_source)                                                  AS traffic_source,
         MAX(country)                                                         AS country,
-
         MAX(CASE WHEN event_name = 'page_view'       THEN 1 ELSE 0 END)     AS reached_page_view,
         MAX(CASE WHEN event_name = 'view_item'       THEN 1 ELSE 0 END)     AS reached_view_item,
         MAX(CASE WHEN event_name = 'add_to_cart'     THEN 1 ELSE 0 END)     AS reached_add_to_cart,
         MAX(CASE WHEN event_name = 'begin_checkout'  THEN 1 ELSE 0 END)     AS reached_checkout,
         MAX(CASE WHEN event_name = 'purchase'        THEN 1 ELSE 0 END)     AS reached_purchase
-
     FROM sessions
-
     {% if is_incremental() %}
         WHERE session_start >= DATE_SUB(CURRENT_DATE(), INTERVAL 3 DAY)
     {% endif %}
-
     GROUP BY session_id, user_id
 ),
 
@@ -38,12 +35,19 @@ funnel_stage AS (
     SELECT
         *,
         CASE
-            WHEN reached_purchase     = 1 THEN '5_purchase'
-            WHEN reached_checkout     = 1 THEN '4_checkout'
-            WHEN reached_add_to_cart  = 1 THEN '3_add_to_cart'
-            WHEN reached_view_item    = 1 THEN '2_view_item'
-            ELSE                               '1_page_view'
-        END AS funnel_stage
+            WHEN reached_purchase     = 1 THEN 'purchase'
+            WHEN reached_checkout     = 1 THEN 'checkout'
+            WHEN reached_add_to_cart  = 1 THEN 'add_to_cart'
+            WHEN reached_view_item    = 1 THEN 'view_item'
+            ELSE                               'page_view'
+        END AS funnel_stage,
+        CASE
+            WHEN reached_purchase     = 1 THEN 5
+            WHEN reached_checkout     = 1 THEN 4
+            WHEN reached_add_to_cart  = 1 THEN 3
+            WHEN reached_view_item    = 1 THEN 2
+            ELSE                               1
+        END AS stage_order
     FROM session_funnel
 )
 
