026a27b856512f3df127be629d11687a00fef143
DcBait <156874072+DcBait@users.noreply.github.com>
2026-06-04 10:09:39 +0800

    Update SQL query to use CAST for funnel_stage

--- dbt_project/cart_abandonment/tests/assert_valid_funnel_stages.sql
+++ dbt_project/cart_abandonment/tests/assert_valid_funnel_stages.sql
@@ -1,8 +1,8 @@
-SELECT funnel_stage
-FROM {{ ref('mart_funnel') }}
-WHERE funnel_stage NOT IN (
+SELECT CAST(funnel_stage AS STRING) AS funnel_stage
+FROM `project-681013b1-bc3e-47ed-b3a`.`cart_abandonment_dev`.`mart_funnel`
+WHERE CAST(funnel_stage AS STRING) NOT IN (
     'page_view',
-    'view_item', 
+    'view_item',
     'add_to_cart',
     'checkout',
     'purchase'
