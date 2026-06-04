d8da1f5e1a004bb9c9dbbc8164ac4a7956f8ea3e
DcBait <156874072+DcBait@users.noreply.github.com>
2026-06-04 13:55:11 +0800

    Update stg_events.sql

--- dbt_project/cart_abandonment/models/staging/stg_events.sql
+++ dbt_project/cart_abandonment/models/staging/stg_events.sql
@@ -8,9 +8,9 @@ WITH raw AS (
         geo.country                             AS country
     FROM {{ source('ga4', 'events_*') }}
     WHERE _TABLE_SUFFIX BETWEEN '20201101' AND '20210131'
-        FORMAT_DATE('%Y%m%d', DATE_SUB(CURRENT_DATE(), INTERVAL 90 DAY))
-        AND
-        FORMAT_DATE('%Y%m%d', CURRENT_DATE())
+    -- NOTE: GA4 public dataset is static (Nov 2020 - Jan 2021)
+    -- In production, replace BETWEEN clause with dynamic 90-day lookback:
+    -- WHERE _TABLE_SUFFIX >= FORMAT_DATE('%Y%m%d', DATE_SUB(CURRENT_DATE(), INTERVAL 90 DAY))
     AND event_name IN (
         'page_view',
         'view_item',
@@ -19,6 +19,5 @@ WITH raw AS (
         'purchase'
     )
 )
-
 SELECT * FROM raw
 WHERE user_id IS NOT NULL
